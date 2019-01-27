86d4ae7d4c47aaecb6f426c1f530eec3ce81218c
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-01-27 12:47:32 +1000

    Update session.php

--- session.php
+++ session.php
@@ -25,6 +25,43 @@ if (intercept('POST')) {
     proxyCall($path);
 }
 
+$curl = curl_init();
+ 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://gateway.test.nab.com.au/api/rest/version/49/merchant/TESTVICTESNB237/session",
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_SSL_VERIFYHOST => false,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "apiOperation=CREATE_SESSION&merchant=TESTVICTESNB237&apiUsername=merchant.TESTVICTESNB237&apiPassword=ed17a6e2ed43172c0f7bde912cbe54de&{\n    \"lineOfBusiness\":\"test_socks\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Basic bWVyY2hhbnQuVEVTVE9QVElDQUw6ZTM3YTg2YzMxMDk4ZWM0YzQ4OTc3YWMxYTNlMzhmNzA=",
+    "Postman-Token: aad070c0-44cf-469f-81e7-0cd8f554b804",
+    "cache-control: no-cache"),
+));
+ 
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+ 
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+
+
+}
+parse_str($response);
+echo "<br>".$merchant."<br>";
+echo $result."<br>";
+echo $session_id."<br>";
+echo $session_updateStatus."<br>";
+echo $session_updateStatus."<br>";
+echo $session_version;
 ?>
 
 <html>
