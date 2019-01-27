487f43f86a15ce28cf6fa182fd4c5009af51e017
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-01-28 07:26:19 +1000

    Add files via upload

---
+++ simplesessionNEW.php
@@ -0,0 +1,38 @@
+<?php
+ 
+$curl = curl_init();
+$username = urlencode(‘merchant.TESTVICTESNB237’);
+$password = urlencode(‘ed17a6e2ed43172c0f7bde912cbe54de’);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://gateway.test.nab.com.au/api/rest/version/46/merchant/TESTVICTESNB237/session",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n\"lineOfBusiness\": \"test_socks\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json',
+    'Authorization: Basic '. base64_encode("$username:$password"),
+    "cache-control: no-cache",
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+/*parse_str($response);
+echo "<br>".$merchant."<br>";
+echo $result."<br>";
+echo $session_id."<br>";
+echo $session_updateStatus."<br>";
+echo $session_updateStatus."<br>";
+echo $session_version;*/
+?>
