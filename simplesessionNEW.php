a96cae34738b4fff1a2541449c1d73e03853a3b4
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-01-28 08:24:59 +1000

    Update simplesessionNEW.php

--- simplesessionNEW.php
+++ simplesessionNEW.php
@@ -5,15 +5,15 @@ $username = ‘merchant.TESTVICTESNB237’;
 $password = ‘ed17a6e2ed43172c0f7bde912cbe54de’;
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://gateway.test.nab.com.au/api/rest/version/46/merchant/TESTVICTESNB237/session",
-   CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_SSL_VERIFYPEER => false,
   CURLOPT_SSL_VERIFYHOST => false,
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
+  CURLOPT_MAXREDIRS => 100,
+  CURLOPT_TIMEOUT => 400,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n\"lineOfBusiness\": \"test_socks\"\n}",
+  CURLOPT_POSTFIELDS => "{\n    \"lineOfBusiness\": \"test_socks\"\n}",
   CURLOPT_HTTPHEADER => array(
     'Content-Type: application/json',
     'Authorization: Basic ' . base64_encode("merchant.$merchantId:$password")
