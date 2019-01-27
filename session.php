20e4f8b79a662819a6fcf500db58b35e17ecabf6
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-01-27 13:41:34 +1000

    Update session.php

--- session.php
+++ session.php
@@ -25,43 +25,6 @@ if (intercept('POST')) {
     proxyCall($path);
 }
 
-$curl = curl_init();
- 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://gateway.test.nab.com.au/api/rest/version/49/merchant/TESTVICTESNB237/session",
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_SSL_VERIFYHOST => false,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS =>"{\n\"lineOfBusiness\": \"test_socks\"\n}",,
-  CURLOPT_HTTPHEADER => array(
-    "Authorization: Basic bWVyY2hhbnQuVEVTVE9QVElDQUw6ZTM3YTg2YzMxMDk4ZWM0YzQ4OTc3YWMxYTNlMzhmNzA=",
-    "Postman-Token: aad070c0-44cf-469f-81e7-0cd8f554b804",
-    "cache-control: no-cache"),
-));
- 
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
- 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-
-
-}
-parse_str($response);
-echo "<br>".$merchant."<br>";
-echo $result."<br>";
-echo $session_id."<br>";
-echo $session_updateStatus."<br>";
-echo $session_updateStatus."<br>";
-echo $session_version;
 ?>
 
 <html>
