10d1f7203c7bbcae1905aa9b192a30ffb964ce9c
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-07-26 12:34:08 +1000

    Update checkoutsessionBAIDURIMPGS.php

--- checkoutsessionBAIDURIMPGS.php
+++ checkoutsessionBAIDURIMPGS.php
@@ -9,7 +9,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 300,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=TEST950029025&apiUsername=merchant.TEST950029025&apiPassword=5aa689a4ff28a8d62e12a20e77133134&order.currency=BND&order.id=546098QIO467W&order.amount=45",
+  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=TEST950029025&apiUsername=merchant.TEST950029025&apiPassword=5aa689a4ff28a8d62e12a20e77133134&order.currency=BND&order.id=546098QI34467W&order.amount=45",
   CURLOPT_HTTPHEADER => array(
     "Authorization: Basic bWVyY2hhbnQuVEVTVFZJQ1RFU05BQjAxOmU5NmYyYjA0YzI4ZGRmN2FhOWZhYjFhYjQ1NGNkNzc1",
     "Content-Type: application/x-www-form-urlencoded",
