62844105ab8d9e5e98d2135370def0243ab66cbf
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-09-13 16:51:03 +1000

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ include '_bootstrap.php';
             <li><a href="./transaction.php">Transaction API</a></li>
             <li><a href="./3DSecure.php">3DSecure API</a></li>
             <li><a href="./Check3DSEnrollment.php">Check for 3DS Enrollment</a></li>
-            <li><a href="./3dsRedirect.php">responseUrl</a></li>
+            <li><a href="./3dsRedirect.html">responseUrl</a></li>
             <li><a href="./simplesessionNEW.php">Create Session</a></li>
             <li><a href="./checkoutsessionBAIDURIMPGS.php">Create Checkout Session</a></li>
             <li><a href="./checkout__BAIDURI_N_950028548_mastercard.php">Pass Checkout Session with Hosted Checkout</a></li>
