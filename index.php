014e08c8069907956b009eba5e80620a2cbd313e
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-09-24 17:36:26 +1000

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,8 @@ include '_bootstrap.php';
             <li><a href="./simplesessionNEW.php">Create Session</a></li>
             <li><a href="./checkoutsessionBAIDURIMPGS.php">Create Checkout Session</a></li>
             <li><a href="./checkout__BAIDURI_N_950028548_mastercard.php">Pass Checkout Session with Hosted Checkout</a></li>
+            <li><a href="./HostedSessionTESTVICTOR01_53.html">Create/Update Session (Hosted Session)</a></li>
+            <li><a href="./threeds2.html">3DS JavaScript API with Session</a></li>
         </ul>
     </body>
 </html>
