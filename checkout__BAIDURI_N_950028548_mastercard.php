bdbf25158d091aaf86802a7da289828e7ae0a95d
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-09-25 12:20:19 +1000

    Update checkout__BAIDURI_N_950028548_mastercard.php

--- checkout__BAIDURI_N_950028548_mastercard.php
+++ checkout__BAIDURI_N_950028548_mastercard.php
@@ -39,10 +39,10 @@ var n = x.toString();
 </script> 
 
 <script type="text/javascript"> 
-    var sessionId = "SESSION0002748494291J55207563E6";
-    var sessionVersion = "b710638c01";
-    var successIndicator = "315a3827f31946b5";
-    var orderId = "546X919GNM2";
+    var sessionId = "SESSION0002579089072M5309534E14";
+    var sessionVersion = "3afb74ba01";
+    var successIndicator = "837232e5b74845fd";
+    var orderId = "546X919ZX";
 var x = Math.floor((Math.random() * 100000) + 1000); 
 var n = x.toString(); 
 function errorCallback(error) { 
