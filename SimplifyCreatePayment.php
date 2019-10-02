c395e3382d378b7205ac08c44c6bd2c7f93dbd35
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-02 17:22:43 +1000

    Update SimplifyCreatePayment.php

--- SimplifyCreatePayment.php
+++ SimplifyCreatePayment.php
@@ -6,7 +6,7 @@ Simplify::$publicKey = 'sbpb_NGUzNDA0MmUtYTdhOS00NDA1LWI3NTUtNTA0YzRiMmY1OTNl';
 Simplify::$privateKey = 'sQYcksCm3TgkBwIqlXu2MkHqImdK7mA7gl53aVjUmJ95YFFQL0ODSXAOkNtXTToq';
  
 $payment = Simplify_Payment::createPayment(array(
-        'amount' => '1000',
+        'amount' => '10000',
         'token' => '[TOKEN ID]',
         'description' => 'payment description',
         'reference' => '7a6ef6be31',
